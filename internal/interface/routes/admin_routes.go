ba02b8b166d3381375f95fd6e6f3cf0c6aec3ce8
Zeamanuel-Admasu <zeamanshibru2020@gmail.com>
2025-04-11 22:40:10 +0300

    My local work before merge

---
+++ internal/interface/routes/admin_routes.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"github.com/Zeamanuel-Admasu/afro-vintage-backend/internal/domain/auth"
+	"github.com/Zeamanuel-Admasu/afro-vintage-backend/internal/interface/controllers"
+	"github.com/Zeamanuel-Admasu/afro-vintage-backend/internal/interface/middlewares"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterAdminRoutes(r *gin.Engine, ctrl *controllers.AdminController, jwtSvc auth.JWTService) {
+	adminGroup := r.Group("/admin")
+	adminGroup.Use(middlewares.AuthMiddleware(jwtSvc), middlewares.AuthorizeRoles("admin"))
+
+	// GET /admin/users?role=
+	adminGroup.GET("/users", ctrl.GetAllUsers)
+
+	// More admin routes can be added here (e.g. transactions, reviews, dashboards, etc.)
+	// adminGroup.GET("/dashboard", ctrl.GetDashboardMetrics)
+	// adminGroup.GET("/transactions", ctrl.GetAllTransactions)
+}
